eb583ed36b5e8bcb50f2fd7c453ccfa1f8522853
spookje <stefan_123_10@hotmail.com>
2015-10-12 16:31:26 +0200

    Begin van het miniproject, met authenticatie en print van het binnenkomende XML bestand.

--- MiniprojectS5.py
+++ MiniprojectS5.py
@@ -1,1 +1,12 @@
-__author__ = 'Stefan'
+__author__ = 'S5 (V1S)'
+
+import requests
+
+auth_details = ('stefan.vantveld@student.hu.nl', 'D1TBMhlZz8dkv5FLZC9RgkVflWjntOvmOXHV6mGDoGvLoeGE-SlOfw')
+
+response = requests.get('http://webservices.ns.nl/ns-api-avt?station=Ut', auth=auth_details)
+
+print(response.text)
+
+
+
