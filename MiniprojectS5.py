ad3f6b2a917ebe571c413c3bf2f80296b8bcac6c
spookje <stefan_123_10@hotmail.com>
2015-10-13 11:14:17 +0200

    Begin met de mogelijkheid van het printen van de apixml in een dict. Dit door het eerst op te slaan als een xml bestandje en deze dan te lezen.

--- MiniprojectS5.py
+++ MiniprojectS5.py
@@ -9,35 +9,15 @@ auth_details = ('stefan.vantveld@student.hu.nl', 'D1TBMhlZz8dkv5FLZC9RgkVflWjntO
 response = requests.get('http://webservices.ns.nl/ns-api-avt?station=Ut', auth=auth_details)
 
 def schrijf_xml():
-    try:
-        bestand = open('testxml', 'w')
-        try:
-            bestand.write(response.text)
-            bestand.close()
-        except:
-            print('Kan bestand niet schrijven')
-        finally:
-            print('klaar (write)')
-    except:
-        print('Kan bestand niet openen')
-    finally:
-        print('klaar')
+    bestand = open('testxml', 'w')
+    bestand.write(response.text)
+    bestand.close()
+
 
 def lees_xml():
-    try:
-        bestand = open('testxml', 'r')
-        try:
-            xml_string = bestand.read()
-            return xmltodict.parse(xml_string)
-        except:
-            print('kan bestand niet lezen/verwerken')
-        finally:
-            bestand.close()
-            print('klaar')
-    except:
-        print('kan bestand niet openen')
-    finally:
-        print('klaar (openen)')
+    bestand = open('testxml', 'r')
+    xml_string = bestand.read()
+    return xmltodict.parse(xml_string)
 
 schrijf_xml()
 api_dict = lees_xml()
