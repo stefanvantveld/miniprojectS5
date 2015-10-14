e8848950fdc1d91e9a4f6ba4bfd1a9173541f4ce
spookje <stefan_123_10@hotmail.com>
2015-10-14 09:22:11 +0200

    De dictionary is in een list gezet als variabele.

--- MiniprojectS5.py
+++ MiniprojectS5.py
@@ -4,31 +4,32 @@ import requests
 import xmltodict
 from tkinter import *
 
-auth_details = ('stefan.vantveld@student.hu.nl', 'D1TBMhlZz8dkv5FLZC9RgkVflWjntOvmOXHV6mGDoGvLoeGE-SlOfw')
-station = input("Over welk station wil je informatie")
+
+
+auth_details = ('stefan.vantveld@student.hu.nl', 'D1TBMhlZz8dkv5FLZC9RgkVflWjntOvmOXHV6mGDoGvLoeGE-SlOfw') #authentication details for logging into the NS-Api
+station = input("Over welk station wil je informatie")  #'station' variable will get the same value as the user input.
 
 response = requests.get('http://webservices.ns.nl/ns-api-avt?station=%s' % station , auth=auth_details)
+#get information from the api, with the station code from 'station' and with the authentication details.
 
-def schrijf_xml():
-    bestand = open('testxml', 'w')
-    bestand.write(response.text)
-    bestand.close()
+def schrijf_xml():  #function to write the response from the API into an XML-file.
+    bestand = open('testxml', 'w')          #opens the xml file to write.
+    bestand.write(str(response.text))       #write the response.
+    bestand.close()                         #close the file.
 
-def lees_xml():
-    bestand = open('testxml', 'r')
-    xml_string = bestand.read()
-    return xmltodict.parse(xml_string)
+def lees_xml():     #function to read the xml file.
+    bestand = open('testxml', 'r')          #opens the xml file to read.
+    xml_string = bestand.read()             #make a variable which reads the xml file.
+    return xmltodict.parse(xml_string)      #returns the xml file as a dictionary.
 
 schrijf_xml()
 api_dict = lees_xml()
-#print(api_dict['ActueleVertrekTijden']['VertrekkendeTrein'])
+list = []
 
-for station in api_dict['ActueleVertrekTijden']['VertrekkendeTrein']:
-    #print(station)
-    for x in station:
-        print(x , station[x])
-window = Tk()
-label = Label(window, text = api_dict['ActueleVertrekTijden']['VertrekkendeTrein'])
-label.pack()
-window.mainloop()
-#print(response.text)
+for station in api_dict['ActueleVertrekTijden']['VertrekkendeTrein']:   #runs through the xml file
+    for x in station:                                                   #runs through every 'VertrekkendeTrein' in the last loop.
+        #print(x, station[x])
+        list.append(x)
+        list.append(station[x])
+
+print(list)
